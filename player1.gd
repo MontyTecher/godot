4654a07bb90613eb17b8ca488ed9f1f6c7fb22ec
user uwu <m.silva600@montytech.net>
2025-03-19 12:59:01 -0400

    we love boys

--- player1.gd
+++ player1.gd
@@ -5,9 +5,9 @@ extends CharacterBody2D
 # Put all of this into _process and follow the 2D tutorial. 
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Input.get_vector("", "", "up1", "down1")
 	velocity = input_direction * speed
 func _physics_process(delta):
-	print("FPS: %s" % [Engine.get_frames_per_second()])
 	get_input()
 	move_and_slide()
+	print("FPS: %s" % [Engine.get_frames_per_second()])
